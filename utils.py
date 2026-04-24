079ca9e4191a371f7d3023144d3a559ef3818b33
Sharl Morlaroll <sharl@hauN.org>
2026-04-24 22:39:42 +0900

    support resource_path

---
+++ utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+
+def resource_path(path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, path)
+    return os.path.join(os.path.abspath('.'), path)
