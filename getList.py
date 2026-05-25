1d76e39c0c934b05b03778f3cd9f07b0df6833aa
Sharl Morlaroll <sharl@hauN.org>
2026-05-25 21:13:06 +0900

    add methods: get_title for startup, get_depth instead of getDepth from calc

--- getList.py
+++ getList.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime as dt
+
 import requests
 
 LIST_URL: str = 'https://www.jma.go.jp/bosai/quake/data/list.json'
@@ -13,14 +15,17 @@ class getList:
         with self.session.get(LIST_URL, timeout=3) as r:
             self.data: dict = r.json()
 
-    def find(self, eid: str) -> dict:
-        """eid から一致するデータを返す"""
+    def find(self, eid: str | None) -> dict:
+        """
+        eid から一致するデータを返す
+        eid: None の場合は最新のデータを返す
+        """
         for j in self.data:
-            if j['eid'] == eid and j['ttl'] == '震源・震度情報':
+            if (j['eid'] == eid or eid is None) and j['ttl'] == '震源・震度情報':
                 return j
         return {}
 
-    def get_maxi_cities(self, eid: str) -> str:
+    def get_maxi_cities(self, eid: str | None) -> str:
         # cities: dict[str, list[str]] = {}
         data: dict = self.find(eid)
 
@@ -48,3 +53,40 @@ class getList:
         #     break
         # return ' '.join(lines)
         return ''
+
+    def get_title(self, eid: str | None) -> str:
+        data = self.find(eid)
+
+        if not eid:
+            eid = data['eid']
+        # 発表時点の震源地
+        region_name = data['anm']
+        # 発表時点のマグニチュード
+        magunitude = data['mag']
+        # 発表時点の震源深さ
+        depth = self.get_depth(data['cod'])
+        # 発表時点の最大震度
+        intensity = data['maxi'].replace('+', '強').replace('-', '弱')
+        loc = self.get_maxi_cities(None)
+
+        lines = [
+            dt.strptime(eid, '%Y%m%d%H%M%S').strftime('%Y/%m/%d %H:%M:%S'),
+            region_name,
+            f"M{magunitude} 深さ {depth}",
+            f"最大震度 {intensity}",
+            loc,
+        ]
+        return '\n'.join(lines).strip()
+
+    def get_depth(self, cod: str) -> str:
+        # '+28.6+129.7+0/',      # ごく浅い
+        # '+38.4+141.9-60000/',  # 60km
+        # '+37.5+138.6/',        # 不明
+        # '',                    # 不明
+        # [+-]lat[+-]lng[+-]depth/   ISO6709
+        parts = cod.removesuffix('/').replace('-', '+').split('+')
+        if len(parts) < 4:
+            return '不明'
+
+        km = int(parts[3]) // 1000
+        return f'{km}km' if km > 0 else 'ごく浅い'
