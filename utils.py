4d5ef7518ee9e4aaaa5ad30169cf8840fb84b2fa
Sharl Morlaroll <sharl@hauN.org>
2026-05-29 04:21:47 +0900

    add type hint

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 
-def resource_path(path):
+def resource_path(path: str) -> str:
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, path)
     return os.path.join(os.path.abspath('.'), path)
